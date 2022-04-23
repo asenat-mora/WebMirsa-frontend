0fd8fb3b27b770ca533420382b2333d555811308
Juan Camilo Villota Ballesteros <38733879+jcvillotab@users.noreply.github.com>
2022-04-22 20:54:27 -0500

    uploading login view

---
+++ src/views/Login.vue
@@ -0,0 +1,33 @@
+<template>
+    <body class="login-body">
+        <div class="login-container">
+            <div class="login-info-container">
+                <h1 class="login-litle">Iniciar Sesión</h1>
+                <form class="login-form">
+                    <input class="login-input" type="email" placeholder="Correo electronico">
+                    <input class="login-input" type="password" placeholder="Contraseña">
+                    <a href="#"> <span class="login-span">Olvidaste tu contraseña?</span> </a>
+
+                    <router-link to="/" class="login-btn">
+                        Login
+                    </router-link>
+
+                    <!--  <button class="login-btn">login</button> -->
+                    <p>No tengo una cuenta
+                        <br>
+                        <a href="formRegisterUser.html">
+                            <span class="login-span">Registrarse</span>
+                        </a>
+                    </p>
+                </form>
+            </div>
+            <img class="login-image-container" src="../assets/img/login/car.jpg" alt="">
+        </div>
+    </body>
+</template>
+
+<script>
+export default{
+    name: 'Login',
+}
+</script>
