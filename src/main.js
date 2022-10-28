bfcb039b3690c7d725ecdb43ccb8dc6405f85593
Juan Camilo Villota Ballesteros <jcvillotab@unal.edu.co>
2022-10-28 11:06:03 -0500

    Fixing quotation list

--- src/main.js
+++ src/main.js
@@ -21,6 +21,7 @@ import Button from 'primevue/button';
 import InputText from 'primevue/inputtext';
 import Dialog from 'primevue/dialog';
 import Menubar from 'primevue/menubar';
+import OrderList from 'primevue/orderlist';
 import 'primevue/resources/themes/saga-blue/theme.css';
 import 'primevue/resources/primevue.min.css';               
 import 'primeicons/primeicons.css'; 
@@ -43,6 +44,7 @@ app.component('Button', Button)
 app.component('InputText', InputText)
 app.component('Dialog', Dialog)
 app.component('Menubar', Menubar)
+app.component('OrderList', OrderList)
 
 
 const store = authStore();
